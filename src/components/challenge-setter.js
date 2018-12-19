767f398d6e33771aa6b19e0268c89b28bcda2b5f
Kevin Ngo <me@ngokevin.com>
2018-12-19 06:56:22 -0800

    work on zip fetch

--- src/components/challenge-setter.js
+++ src/components/challenge-setter.js
@@ -1,14 +1,18 @@
 const utils = require('../utils');
+import ZipLoader from 'zip-loader';
 
 AFRAME.registerComponent('challenge-setter', {
   play: function () {
-    // ID parameter.
     const idParam = AFRAME.utils.getUrlParameter('id');
+    const difficultyParam = AFRAME.utils.getUrlParameter('difficulty');
+
     if (idParam) {
-      this.setChallenge(
-        idParam,
-        AFRAME.utils.getUrlParameter('difficulty')
-      );
+      if (idParam.indexOf('-') !== -1) {
+        // Fetch ZIP file directly.
+        this.fetchZip(idParam, difficultyParam);
+      } else {
+        this.setChallenge(idParam, difficultyParam);
+      }
       return;
     }
 
@@ -26,5 +30,37 @@ AFRAME.registerComponent('challenge-setter', {
       ));
     });
     xhr.send();
+  },
+
+  fetchZip: function (id, difficulty) {
+    const loader = new ZipLoader(`https://beatsaver.com/download/${id}`);
+    loader.on('load', () => {
+      let imageBlob;
+      let songBlob;
+      const event = {
+        beats: '',
+        image: '',
+        info: '',
+        song: ''
+      };
+
+      Object.keys(loader.files).forEach(filename => {
+        if (filename.endsWith('info.json')) {
+          event.info = loader.extractAsJSON(filename);
+        }
+        if (filename.endsWith(`${difficulty}.json`)) {
+          event.beats = loader.extractAsJSON(filename);
+        }
+        if (filename.endsWith(`cover.jpg`)) {
+          event.image = loader.extractAsBlobUrl(filename, 'image/jpg');
+        }
+        if (filename.endsWith(`song.ogg`)) {
+          songBlob = loader.extractAsBlobUrl(filename, 'audio/ogg');
+        }
+      });
+
+      this.el.emit('challengesetfromzip', event);
+    });
+    loader.load();
   }
 });
