ff8bb3564d747301dfde0669e9a073fd800e1d9d
Kevin Ngo <ktngo09@gmail.com>
2019-11-21 22:18:57 -0800

    lower stage, decrease beat hit sound

--- src/components/beat-hit-sound.js
+++ src/components/beat-hit-sound.js
@@ -3,7 +3,7 @@ var sourceCreatedCallback;
 const LAYER_BOTTOM = 'bottom';
 const LAYER_MIDDLE = 'middle';
 const LAYER_TOP = 'top';
-const VOLUME = 0.2;
+const VOLUME = 0.05;
 
 // Allows for modifying detune. PR has been sent to three.js.
 THREE.Audio.prototype.play = function () {
@@ -156,3 +156,20 @@ AFRAME.registerComponent('beat-hit-sound', {
     }
   }
 });
+
+function debounce (func, wait, immediate) {
+  var timeout;
+
+  return function executedFunction () {
+    let context = this;
+    let args = arguments;
+    let later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
